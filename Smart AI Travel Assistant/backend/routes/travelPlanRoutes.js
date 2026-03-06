1ddbc46261facee3c8723c6393f4c7d15166416a
dipali0112 <dipalijoshi0112@gmail.com>
2026-03-06 12:41:52 +0530

    Remove .env from repository

--- Smart AI Travel Assistant/backend/routes/travelPlanRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const travelPlanController = require('../controllers/travelPlanController');
-const authMiddleware = require('../middleware/authMiddleware');
-
-router.post('/', authMiddleware, travelPlanController.getTravelPlan);
-
-module.exports = router;
